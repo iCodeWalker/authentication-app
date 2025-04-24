b2cfb4103cd2d69d7f67a88a9b92992acb792ca3
iCodeWalker <vaibhav3kadwey@gmail.com>
2025-04-25 00:34:25 +0530

    added : switching auth mode with query parameters

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
-import AuthForm from '@/components/auth-form';
+import AuthForm from "@/components/auth-form";
 
-export default async function Home() {
-  return <AuthForm />;
+export default async function Home({ searchParams }) {
+  // #### searchParams is a special prop automatically added to all the page components in next js.
+  // #### It is an object that have one key for any query parameter that is available in the current active URL.
+  const formMode = searchParams.mode || "login";
+  return <AuthForm mode={formMode} />;
 }
--- components/auth-form.js
+++ components/auth-form.js
@@ -4,7 +4,7 @@ import { useFormState } from "react-dom";
 
 import { signUp } from "@/actions/auth-actions";
 
-export default function AuthForm() {
+export default function AuthForm({ mode }) {
   const [formState, formAction] = useFormState(signUp, {});
 
   return (
@@ -28,10 +28,17 @@ export default function AuthForm() {
         </ul>
       )}
       <p>
-        <button type="submit">Create Account</button>
+        <button type="submit">
+          {mode === "login" ? "Login" : "Create Account"}
+        </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        {mode === "login" && (
+          <Link href="/?mode=signup">Create an account.</Link>
+        )}
+        {mode === "signup" && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
