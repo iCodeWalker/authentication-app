8ad35051db792ce3874d7dacb6a9d3fa9aee1bec
iCodeWalker <vaibhav3kadwey@gmail.com>
2025-05-03 14:30:51 +0530

    added : triggering different server actions

--- components/auth-form.js
+++ components/auth-form.js
@@ -2,10 +2,10 @@
 import Link from "next/link";
 import { useFormState } from "react-dom";
 
-import { signUp } from "@/actions/auth-actions";
+import { auth, signUp } from "@/actions/auth-actions";
 
 export default function AuthForm({ mode }) {
-  const [formState, formAction] = useFormState(signUp, {});
+  const [formState, formAction] = useFormState(auth.bind(null, mode), {});
 
   return (
     <form id="auth-form" action={formAction}>
